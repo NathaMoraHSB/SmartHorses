c39992a44863ba2e21fc48310d3ce2b15b19a5df
Nathalia Carolina Mora Arciniegas <166132592+NathaMoraHSB@users.noreply.github.com>
2024-11-25 19:07:30 -0500

    old files deleted

--- src/functions/encontrar_posicion_caballo.py
+++
@@ -1,9 +0,0 @@
-# encontrar_posicion_caballo.py
-
-
-def encontrar_posicion_caballo(matriz, valor_caballo):
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            if matriz[i][j] == valor_caballo:
-                return i, j
-    return None  # Si no se encuentra el caballo
